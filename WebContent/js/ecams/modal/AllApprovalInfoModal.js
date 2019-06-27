f68a666788307b230d217d385787be8773e7210d
jybang <jybang@azsoft.kr>
2019-06-28 08:54:18 +0900

    [관리자] > [결재정보] > 전체조회 modal

---
+++ WebContent/js/ecams/modal/AllApprovalInfoModal.js
@@ -0,0 +1,177 @@
+/**
+ * [결재정보 > 전체조회] 팝업 기능정의
+ * 
+ * <pre>
+ * 	작성자	: 방지연
+ * 	버전 		: 1.0
+ *  수정일 	: 2019-06-25
+ * 
+ */
+
+var userName 	= window.parent.userName;		// 접속자 Name
+var userId 		= window.parent.userId;			// 접속자 ID
+var adminYN 	= window.parent.adminYN;		// 관리자여부
+var userDeptName= window.parent.userDeptName;	// 부서명
+var userDeptCd 	= window.parent.userDeptCd;		// 부서코드
+
+var grdAllApprovalInfo		= new ax5.ui.grid();
+
+var grdAllApprovalInfoData 	= [];
+var cboSysData				= [];
+var cboReqCdData			= [];
+
+var tmpInfo     			= new Object(); 
+var tmpInfoData 			= new Object();
+
+$('input.checkbox-pie').wCheck({theme: 'square-inset blue', selector: 'checkmark', highlightLabel: true});
+
+grdAllApprovalInfo.setConfig({
+    target: $('[data-ax5grid="grdAllApprovalInfo"]'),
+    sortable: true, 
+    multiSort: true,
+    showRowSelector: false,
+    header: {
+        align: "center",
+        columnHeight: 30
+    },
+    body: {
+        columnHeight: 25,
+        onClick: function () {
+            this.self.select(this.dindex);
+        },
+        onDBLClick: function () {},
+    	trStyleClass: function () {},
+    	onDataChanged: function(){
+    		this.self.repaint();
+    	}
+    },
+    columns: [
+        {key: "reqname", 		label: "결재유형",  		width: 120},
+        {key: "cm_sysmsg",		label: "시스템",  		width: 100},
+        {key: "manid", 			label: "직원구분",  		width: 80},
+        {key: "cm_seqno", 		label: "순서",  			width: 60},
+        {key: "cm_name", 		label: "단계명칭",  		width: 150},
+        {key: "cm_codename",	label: "결재구분",  		width: 100},
+        {key: "common", 		label: "정상",  			width: 100},
+        {key: "blank", 			label: "부재",  			width: 100},
+        {key: "holi", 			label: "업무후",  		width: 100},
+        {key: "emg", 			label: "긴급",  			width: 100},
+        {key: "emg2", 			label: "긴급[후]",  		width: 100},
+        {key: "deptcd", 		label: "결재조직",  		width: 100},
+        {key: "rgtcd", 			label: "결재직무",  		width: 100},
+        {key: "rsrccd", 		label: "프로그램종류",		width: 100},
+        {key: "pgmtype", 		label: "프로그램등급", 		width: 100}
+    ]
+});
+
+$('[data-ax5select="cboTeam"]').ax5select({
+	options: []
+});
+
+
+$('input:radio[name^="userRadio"]').wRadio({theme: 'circle-radial red', selector: 'checkmark'});
+
+$(document).ready(function() {
+	getCodeInfo();
+	getSysInfo();
+	
+	$('#chkMember').wCheck("check", false);
+	$('#chkOutsourcing').wCheck("check", false);
+	
+	// 조회
+	$('#btnQry').bind('click', function() {
+		btnQry_Click();
+	});
+	
+	// 닫기
+	$('#btnExit').bind('click', function() {
+		btnExit_Click();
+	});
+});
+
+function getCodeInfo(){
+	var codeInfos = getCodeInfoCommon([
+		new CodeInfo('REQUEST', 'ALL','N')
+		]);
+	
+	cboReqCdData = codeInfos.REQUEST;
+	
+	$('[data-ax5select="cboReqCd"]').ax5select({
+        options: injectCboDataToArr(cboReqCdData, 'cm_micode' , 'cm_codename')
+   	});
+}
+
+function getSysInfo() {
+	tmpInfo = new Object();
+	tmpInfo.userId = userId;
+	tmpInfo.selMsg = "ALL";
+	tmpInfo.closeYn = "N";
+	tmpInfo.sysCd = "";
+	
+	tmpInfoData = new Object();
+	tmpInfoData = {
+		tmpInfo		: tmpInfo,
+		requestType	: 'GETSYSINFO_RPT'
+	}
+	ajaxAsync('/webPage/administrator/ApprovalInfoServlet', tmpInfoData, 'json', successSysInfo);
+}
+
+
+function successSysInfo(data) {
+	cboSysData = data;
+	$('[data-ax5select="cboSys"]').ax5select({
+        options: injectCboDataToArr(cboSysData, 'cm_syscd' , 'cm_sysmsg')
+   	});
+}
+
+function btnQry_Click() {
+	var strSys = "";
+	var strReq = "";
+	var strMem = "";
+	
+	grdAllApprovalInfoData 	= [];
+	grdAllApprovalInfo.setData([]);
+	
+	if(!$('#chkMember').is(':checked') && !$('#chkOutsourcing').is(':checked')) {
+		dialog.alert('직원/외주구분을 선택하여 주시기 바랍니다.',function(){});
+		return;
+	}
+	
+	if(getSelectedIndex('cboSys') > 0) {
+		strSys = getSelectedVal('cboSys').value;
+	}
+	
+	if(getSelectedIndex('cboReqCd') > 0) {
+		strReq = getSelectedVal('cboReqCd').value;
+	}
+	
+	if($('#chkMember').is(':checked') && $('#chkOutsourcing').is(':checked')) {
+		strMem = "9";
+	}else if($('#chkMember').is(':checked')) {
+		strMem = "1";
+	}else {
+		strMem = "2";
+	}
+	
+	tmpInfo = new Object();
+	tmpInfo.SysCd = strSys;
+	tmpInfo.ReqCd = strReq;
+	tmpInfo.ManId = strMem;
+	tmpInfo.SeqNo = "";
+	
+	tmpInfoData = new Object();
+	tmpInfoData = {
+		tmpInfo		: tmpInfo,
+		requestType	: 'GETAPPROVALINFO'
+	}
+	ajaxAsync('/webPage/administrator/ApprovalInfoServlet', tmpInfoData, 'json', successApprovalInfo);
+}
+
+function successApprovalInfo(data) {
+	grdAllApprovalInfoData = data;
+	grdAllApprovalInfo.setData(grdAllApprovalInfoData);
+}
+
+function btnExit_Click() {
+	window.parent.allApprovalInfoModal.close();
+}
---
+++ WebContent/webPage/modal/AllApprovalInfoModal.jsp
@@ -0,0 +1,58 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:import url="/webPage/common/common.jsp" />
+
+<section>
+	<div class="hpanel">
+		<div class="panel-heading">
+            <div class="panel-tools width-5">
+                <a class="closebox width-100" onclick="btnExit_Click()"><i class="fa fa-times"></i></a>
+            </div>
+			[결재정보 전체조회]
+        </div>
+        <div class="panel-body">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="col-xs-3">
+						<div class="col-xs-4">
+							<label>시스템</label>
+						</div>
+						<div class="col-xs-8">
+							<div id="cboSys" data-ax5select="cboSys" data-ax5select-config="{size:'sm',theme:'primary'}" style="width:100%;" ></div>
+						</div>
+					</div>
+					
+					<div class="col-xs-3">
+						<div class="col-xs-4">
+							<label>결재종류</label>
+						</div>
+						<div class="col-xs-8">
+							<div id=cboReqCd data-ax5select="cboReqCd" data-ax5select-config="{size:'sm',theme:'primary'}" style="width:100%;" ></div>
+						</div>
+					</div>
+					
+					<div class="col-xs-3">
+						<input type="checkbox" class="checkbox-pie" id="chkMember" data-label="직원"/>
+				    	<input type="checkbox" class="checkbox-pie" id="chkOutsourcing" data-label="외주"/>
+					</div>
+					
+					<div class="col-xs-3">
+						<button class="btn btn-default" id="btnQry">조회</button>
+						<button class="btn btn-default" id="btnExit">닫기</button>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-xs-12">
+					<div data-ax5grid="grdAllApprovalInfo" data-ax5grid-config="{showLineNumber: true, lineNumberColumnWidth: 40}" style="height: 80%;"></div>
+				</div>
+			</div>
+        </div>
+    </div>
+</section>
+
+<c:import url="/js/ecams/common/commonscript.jsp" />	
+<script type="text/javascript" src="<c:url value="/js/ecams/modal/AllApprovalInfoModal.js"/>"></script>
