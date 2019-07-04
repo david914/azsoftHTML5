553153a97979d15df52959f11da958eaa5e88b94
김대호 <destiny1017@nate.com>
2019-07-04 15:27:35 +0900

    김대호 07/04 15:26 개발/SR기준(집계), 프로그램목록 수정

---
+++ WebContent/js/ecams/report/SRStandardReport.js
@@ -0,0 +1,185 @@
+var SecuYn = null;
+var userid 		= 'MASTER';
+//var userid 		= window.top.userId;
+var picker = [new ax5.ui.picker(), new ax5.ui.picker()];
+var mainGrid = new ax5.ui.grid();
+var columnData = 
+	[ 
+		{key : "ownerdept",label : "요청본부",align : "center",width: "10%"}, 
+		{key : "dept1",label : "요청부서",align : "center",width: "10%"}, 
+		{key : "dept2",label : "개발부서",align : "center",width: "10%"}, 
+		{key : "sysmsg",label : "시스템",align : "center",width: "15%"}, 
+		{key : "chkDate",label : "기간",align : "center",width: "15%"}, 
+		{key : "CntSum",label : "합계",align : "right",width: "40%"} 
+	];
+
+//picker세팅
+for(var i = 0; i <= 1; i++) {
+	picker[i].bind({
+		target: $('[data-ax5picker="picker' + (i+1) + '"]'),
+		direction: "top",
+		content: {
+			width: 220,
+			margin: 10,
+			type: 'date',
+			config: {
+				control: {
+					left: '<i class="fa fa-chevron-left"></i>',
+					yearTmpl: '%s',
+					monthTmpl: '%s',
+					right: '<i class="fa fa-chevron-right"></i>'
+				},
+				dateFormat: 'yyyy/MM/dd',
+				lang: {
+					yearTmpl: "%s년",
+					months: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+					dayTmpl: "%s"
+				}
+			},
+			formatter: {
+				pattern: 'date'
+			}
+		},
+		onStateChanged: function () {
+		},
+		btns: {
+			today: {
+				label: "Today", onClick: function () {
+					var today = new Date();
+					this.self
+					.setContentValue(this.item.id, 0, ax5.util.date(today, {"return": "yyyy/MM/dd"}))
+					.setContentValue(this.item.id, 1, ax5.util.date(today, {"return": "yyyy/MM/dd"}))
+					.close();
+				}
+			},
+			thisMonth: {
+				label: "This Month", onClick: function () {
+					var today = new Date();
+					this.self
+					.setContentValue(this.item.id, 0, ax5.util.date(today, {"return": "yyyy/MM/01"}))
+					.setContentValue(this.item.id, 1, ax5.util.date(today, {"return": "yyyy/MM"})
+							+ '/'
+							+ ax5.util.daysOfMonth(today.getFullYear(), today.getMonth()))
+							.close();
+				}
+			},
+			ok: {label: "Close", theme: "default"}
+		}
+	});	
+}
+
+$(document).ready(function() {
+	
+	mainGrid.setConfig({
+		header : { align: "center" },
+		target : $('[data-ax5grid="mainGrid"]'),
+		showLineNumber : true,
+		showRowSelector : false,
+		multipleSelect : false,
+		lineNumberColumnWidth : 40,
+		rowSelectorColumnWidth : 27,
+		body : {
+			columnHeight: 24,
+	        onClick: function () {
+	        	this.self.clearSelect();
+	            this.self.select(this.dindex);
+	        },
+	        trStyleClass: function () {
+	        	if (this.item.gbn === 'T'){
+	        		return "font-blue";
+	        	} else if (this.item.gbn === 'S'){
+	        		return "font-red";
+	        	}
+	        }
+		},
+     	onDataChanged: function(){
+     	    this.self.repaint();
+     	},
+		columns : columnData
+	});
+	isAdmin();
+	comboSet();
+})
+
+//콤보 데이터 셋
+function comboSet() {
+	
+	var ajaxResult = new Array();
+	var comboData = new Array([],[],[],[]);
+	var ajaxData = new Object();
+	var selectMenu = ["devDept", "reqDept",  "dateStd", "srStat"];
+	
+	//개발부서
+	ajaxData.requestType = 'getTeamInfo1';
+	ajaxResult.push(ajaxCallWithJson('/webPage/report/SRStandardReport', ajaxData, 'json'));
+	
+	//요청부서
+	ajaxData.requestType = 'getTeamInfo2';	
+	ajaxResult.push(ajaxCallWithJson('/webPage/report/SRStandardReport', ajaxData, 'json'));	
+	
+	//조회구분, SR상태
+	ajaxData.requestType = 'getCodeInfo';
+	ajaxResult.push(ajaxCallWithJson('/webPage/report/SRStandardReport', ajaxData, 'json'));
+	
+	ajaxResult[3] = {}; //콤보세팅 4번 반복을 위한 배열 길이 증가용 값
+
+	//콤보박스에 들어갈 데이터 세팅
+	$.each(ajaxResult, function(index, value) {
+		$.each(value, function(key, value2) {
+			if(index == 0 || index == 1) comboData[index].push({value : key == 0 ? null : value2.cm_deptcd, text : value2.cm_deptname});		
+			if(index == 2 && value2.cm_macode == 'RPTPRGGB') comboData[2].push({value : key == 0 ? null : value2.cm_micode, text : value2.cm_codename});		
+			if(index == 2 && value2.cm_macode == 'ISRSTA') comboData[3].push({value : key == 0 ? null : value2.cm_micode, text : value2.cm_codename});					
+		})
+//		comboData[index][0].value = null; //"전체"값은 null로 세팅
+		if(index == 2) comboData[2].splice(0,1); //조회구분의 '전체'항목 삭제
+		
+		$('[data-ax5select="' + selectMenu[index] + '"]').ax5select({
+			options: comboData[index]
+		});	
+	})
+	console.log(comboData);
+}
+
+function isAdmin() {
+	var ajaxData = {
+			requestType : 'isAdmin',
+			UserId : userid
+	}
+	ajaxResultData = ajaxCallWithJson('/webPage/report/SRStandardReport', ajaxData, 'json');
+	SecuYn = ajaxResultData ? "Y" : "N"; 
+}
+
+//picker에 오늘 날짜 디폴트로 세팅
+$(function() {
+	var today = new Date().toISOString().substring(0,10).replace(/-/gi, "/");
+	$("#datEdD").val(today);
+	$("#datStD").val(today);
+})
+
+//조회 클릭 시
+$("#btnSearch").bind('click', function() {
+	
+	var inputData = {	
+		dateGbn : $("[data-ax5select='dateStd']").ax5select("getValue")[0].value,
+		strDate : replaceAllString($("#datStD").val(), '/', ''),
+		endDate : replaceAllString($("#datEdD").val(), '/', ''),
+		deptGbn : "D",
+		dept1 : $("[data-ax5select='reqDept']").ax5select("getValue")[0].value,
+		dept2 : $("[data-ax5select='devDept']").ax5select("getValue")[0].value,
+		Sta : $("[data-ax5select='devDept']").ax5select("getValue")[0].value,
+		srid : $("#srId").val() == '' ? null : $("#srId").val()
+	}
+	
+	ajaxData = {
+			inputData : inputData,
+			userId : userid,
+			reqCd : "01",
+			secuYn : SecuYn,
+			requestType : "getProgCnt"
+	}
+	
+	console.log(ajaxData);
+	var ajaxResult = ajaxCallWithJson('/webPage/report/SRStandardReport', ajaxData, 'json');
+	mainGrid.setData(ajaxResult);
+//	console.log(ajaxResult);
+})
--- src/html/app/report/SRStandardReport.java
+++ src/html/app/report/SRStandardReport.java
@@ -1,6 +1,7 @@
 package html.app.report;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
@@ -14,6 +15,10 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import app.common.CodeInfo;
+import app.common.TeamInfo;
+import app.common.UserInfo;
+import app.eCmp.Cmp1400;
 import html.app.common.ParsingCommon;
 
 @WebServlet("/webPage/report/SRStandardReport")
@@ -23,6 +28,10 @@ public class SRStandardReport extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	Gson gson = new Gson();
+	UserInfo userinfo = new UserInfo();
+	TeamInfo teaminfo = new TeamInfo();
+	CodeInfo codeinfo = new CodeInfo();
+	Cmp1400 cmp1400 = new Cmp1400();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -42,8 +51,20 @@ public class SRStandardReport extends HttpServlet {
 			response.setCharacterEncoding("UTF-8");
 			
 			switch (requestType) {
-				case "" :
-					response.getWriter().write("");
+				case "getTeamInfo1" :
+					response.getWriter().write( getTeamInfo1() );
+					break;
+				case "getTeamInfo2" :
+					response.getWriter().write( getTeamInfo2() );
+					break;
+				case "getCodeInfo" :
+					response.getWriter().write( getCodeInfo() );
+					break;
+				case "isAdmin" :
+					response.getWriter().write( isAdmin(jsonElement) );
+					break;
+				case "getProgCnt" :
+					response.getWriter().write( getProgCnt(jsonElement) );
 					break;
 				default:
 					break;
@@ -53,5 +74,31 @@ public class SRStandardReport extends HttpServlet {
 		} finally {
 		}
 		
+	}
+	
+	private String isAdmin(JsonElement jsonElement) throws SQLException, Exception {
+		String user = null;
+		user = ParsingCommon.jsonStrToStr( ParsingCommon.jsonEtoStr(jsonElement, "UserId") );
+		return gson.toJson(userinfo.isAdmin(user));
+	}
+	
+	private String getTeamInfo1() throws SQLException, Exception {
+		return gson.toJson(teaminfo.getTeamInfoGrid2("ALL", "Y", "DEPT", "N"));
+	}
+	
+	private String getTeamInfo2() throws SQLException, Exception {
+		return gson.toJson(teaminfo.getTeamInfoGrid2("ALL", "Y", "req", "N"));
+	}
+	
+	private String getCodeInfo() throws SQLException, Exception {
+		return gson.toJson(codeinfo.getCodeInfo("RPTPRGGB,ISRSTA", "ALL", "N"));
+	}
+	
+	private String getProgCnt(JsonElement jsonElement) throws SQLException, Exception {
+		HashMap<String, String>	DataInfoMap = ParsingCommon.jsonStrToMap( ParsingCommon.jsonEtoStr(jsonElement, "inputData") );
+		String userid = ParsingCommon.jsonStrToStr( ParsingCommon.jsonEtoStr(jsonElement, "userId"));
+		String reqCd = ParsingCommon.jsonStrToStr( ParsingCommon.jsonEtoStr(jsonElement, "reqCd"));
+		String secuYn = ParsingCommon.jsonStrToStr( ParsingCommon.jsonEtoStr(jsonElement, "secuYn"));
+		return gson.toJson( cmp1400.getProgCnt(userid, reqCd, secuYn, DataInfoMap) );
 	}
 }
