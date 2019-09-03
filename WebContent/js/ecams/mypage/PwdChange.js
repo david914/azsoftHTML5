59183477efc4652fb62f236cafac3e087ceb8a39
david914 <shlee@azsoft.kr>
2019-09-03 13:33:38 +0900

    비밀번호 변경 edge일때도 문제없게 수정

--- WebContent/js/ecams/mypage/PwdChange.js
+++ WebContent/js/ecams/mypage/PwdChange.js
@@ -16,7 +16,7 @@ var userDeptCd 	= window.parent.userDeptCd;		// 부서코드
 // 브라우저 검사
 var agent = navigator.userAgent.toLowerCase();
 
-if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {	//익스일떄
+if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) || (agent.indexOf("edge") != -1)) {	//익스일떄
 	$("#txtPw").prop("type", "password");
 	$("#txtUpdatePw1").prop("type", "password");
 	$("#txtUpdatePw2").prop("type", "password");
