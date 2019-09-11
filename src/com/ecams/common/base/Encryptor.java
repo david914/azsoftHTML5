9e2d6c21bbf6ec6648d8f3a2339bf669fabae465
cishappy <cishappy@DESKTOP-O8OGV07>
2019-09-11 10:44:23 +0900

    프로퍼티관리 암호화방식 변경 (DES-->AES256) 암호화모듈추가 (AES,AES256)

--- src/com/ecams/common/base/Encryptor.java
+++ src/com/ecams/common/base/Encryptor.java
@@ -11,18 +11,31 @@
 /******************************************************************************/
 package com.ecams.common.base;
 
+import java.security.GeneralSecurityException;
+import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
+import com.ecams.common.logger.EcamsLogger;
+
+
 public class Encryptor 
 {	
+	Logger ecamsLogger  = EcamsLogger.getLoggerInstance();
   	private static SecretKeySpec secretKeySpec;
-	private static String skey = "GISDESKY";
+	private static String skey = "ecams_secret_pwd";
+	//private static String skey = "GISDESKY";
   	
 	private static BASE64Encoder encoder;
 	private static BASE64Decoder decoder;
@@ -115,7 +128,93 @@ public class Encryptor
 		}
 		return SHA;
 	}
-  	
+
+  	public String strGetEncrypt_AES(String str) throws Exception
+  	{
+  		try {
+	  		//https://offbyone.tistory.com/286
+	  		byte[] secretKey = skey.getBytes();
+	        byte[] iv = skey.getBytes();
+	            	  
+		    SecretKey key = new SecretKeySpec(secretKey, "AES");
+		    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		    IvParameterSpec ivSpec = new IvParameterSpec(iv);
+		    cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+		    
+		    byte[] results = cipher.doFinal(str.getBytes());
+	        BASE64Encoder encoder = new BASE64Encoder();
+	
+	        return encoder.encode(results);
+  		} catch (Exception exception) {
+			exception.printStackTrace();
+			ecamsLogger.error("## Encryptor.strGetEncrypt_AES256() Exception START ##");
+			ecamsLogger.error("## Error DESC : ", exception);
+			ecamsLogger.error("## Encryptor.strGetEncrypt_AES256() Exception END ##");
+			throw exception;
+		}finally{
+		}
+  	}
+  	public String strGetEncrypt_AES256(String str) throws NoSuchAlgorithmException,GeneralSecurityException,UnsupportedEncodingException
+  	{
+		String iv;
+		Key keySpec;
+		
+		iv = skey.substring(0,16);
+  		byte[] keyBytes = new byte[16];
+  		byte[] b = skey.getBytes("UTF-8");
+  		int len = b.length;
+  		if (len > keyBytes.length) {
+  			len = keyBytes.length;
+  		}
+  		System.arraycopy(b,0,keyBytes,0,len);
+  		SecretKey key = new SecretKeySpec(keyBytes, "AES");
+  		
+  		keySpec = key;
+  		
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
+		byte[] encrypted = cipher.doFinal(str.getBytes("UTF-8"));
+		String enStr = new String(Base64.encodeBase64(encrypted));  			
+
+        return enStr;
+  	}
+  	public String strGetDecrypt_AES(String str) throws Exception
+	{  		
+
+        byte[] secretKey = skey.getBytes();
+        byte[] iv = skey.getBytes();
+        
+        SecretKey key = new SecretKeySpec(secretKey, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+        
+        byte[] decrypted = cipher.doFinal(new BigInteger(str, 16).toByteArray());
+        
+        return new String(decrypted);
+	}  
+	public String strGetDecrypt_AES256(String str) throws NoSuchAlgorithmException,GeneralSecurityException,UnsupportedEncodingException
+	{  		
+		String iv;
+		Key keySpec;
+		
+		iv = skey.substring(0,16);
+  		byte[] keyBytes = new byte[16];
+  		byte[] b = skey.getBytes("UTF-8");
+  		int len = b.length;
+  		if (len > keyBytes.length) {
+  			len = keyBytes.length;
+  		}
+  		System.arraycopy(b,0,keyBytes,0,len);
+  		SecretKey key = new SecretKeySpec(keyBytes, "AES");
+  		
+  		keySpec = key;
+  		
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
+		byte[] byteStr = Base64.decodeBase64(str.getBytes());			
+
+        return new String(cipher.doFinal(byteStr),"UTF-8");
+	} 	
   	public static void main(String[] args)
   	{
   		try
